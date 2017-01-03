9e519186c8f6dda4e71f2b99ac4d81f9842645b8
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-01-03 12:36:59 -0300

    mini fix

--- src/pages/HelloWorld/HelloWorldReducer.ts
+++ src/pages/HelloWorld/HelloWorldReducer.ts
@@ -5,13 +5,9 @@ import { HelloWorldState, helloWorldState } from './HelloWorldState';
 export function helloWorldReducer(state: HelloWorldState = helloWorldState, action: Action): HelloWorldState {
   switch (action.type) {
     case 'increase':
-      return {
-        count: state.count + 1
-      };
+      return Object.assign({}, state, { count: state.count + 1});
     case 'decrease':
-      return {
-        count: state.count - 1
-      };
+      return Object.assign({}, state, { count: state.count - 1});
     default:
       return state;
   }
