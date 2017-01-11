1e30939fe595b71b1baab64c2afe4bf1503ab538
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-01-11 11:23:02 -0500

    minirefactor

--- src/reducers/FetchReducer.ts
+++ src/reducers/FetchReducer.ts
@@ -3,9 +3,7 @@ import Actions from '../actions';
 export default function fetchReducer(state: any, action: any) {
   switch (action.type) {
     case Actions.fetchResponseSuccess:
-      const newState = { ...state };
-      newState[action.url] = action.body;
-      return newState;
+      return { ...state, [action.url]: action.body }
   }
   return state || {};
 }
