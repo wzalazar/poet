92b7886c9c9b451dff09d94dfff8be89600734ee
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-01-17 16:32:39 -0500

    modals/login styling

--- src/modals/Login.tsx
+++ src/modals/Login.tsx
@@ -2,12 +2,15 @@
 declare var require: (moduleId: string) => any;
 
 import * as React from 'react';
+import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { Action } from 'redux';
 
 const { Modal } = require('react-overlays');
 
 import Actions from '../actions';
+
+import './Login.scss';
 
 interface LoginActions {
   dispatchLoginModalDisposeRequested: () => Action;
@@ -35,6 +38,7 @@ const dialogStyle = () => {
   const left = 50;
   const top = 50;
   return {
+    textAlign: 'center',
     position: 'absolute',
     width: 400,
     top: top + '%', left: left + '%',
@@ -55,9 +59,27 @@ function render(props: LoginProps & LoginActions) {
       show={props.visible}
       onHide={props.dispatchLoginModalDisposeRequested}
     >
-      <div style={dialogStyle()}>
-        <h1>Test</h1>
-        <button className='btn btn-default' onClick={props.dispatchLoginResponse}>Login</button>
+      <div className="modal-login" style={dialogStyle()}>
+        <h1>Login to Poet</h1>
+        <div>
+          <img onClick={props.dispatchLoginResponse}
+               src="http://www.qr-code-generator.com/phpqrcode/getCode.php?cht=qr&chl=http%3A%2F%2Fwww.po.et&chs=180x180&choe=UTF-8&chld=L|0"/>
+        </div>
+        <div className="mb-2">Scan the QR code to login</div>
+        <div className="onboard mb-2">
+          <div className="placeholder-box" />
+          <div className="ml-2">
+            <div><Link to="http://www.po.et/" >Download</Link> the Poet: Authorizer App</div>
+            <div className="text-muted">Average onboard takes &lt; 5 minutes</div>
+          </div>
+        </div>
+        <div className="scan">
+          <div className="placeholder-box" />
+          <div className="ml-2">
+            <div>Scan the QR code</div>
+            <div className="text-muted">Login to the app &gt; scan QR code</div>
+          </div>
+        </div>
       </div>
     </Modal>
   );
