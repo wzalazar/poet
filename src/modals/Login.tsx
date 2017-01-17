9cba39f3e83a41ad20956609f46955bd50bc8692
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-01-17 17:11:29 -0500

    refactor: modals/login: use scss instead of inlined styles

--- src/modals/Login.tsx
+++ src/modals/Login.tsx
@@ -21,45 +21,16 @@ interface LoginProps {
   visible: boolean;
 }
 
-const modalStyle = {
-  position: 'fixed',
-  zIndex: 1040,
-  top: 0, bottom: 0, left: 0, right: 0
-};
-
-const backdropStyle = {
-  ...modalStyle,
-  zIndex: 'auto',
-  backgroundColor: '#000',
-  opacity: 0.5
-};
-
-const dialogStyle = () => {
-  const left = 50;
-  const top = 50;
-  return {
-    textAlign: 'center',
-    position: 'absolute',
-    width: 400,
-    top: top + '%', left: left + '%',
-    transform: `translate(-${top}%, -${left}%)`,
-    border: '1px solid #e5e5e5',
-    backgroundColor: 'white',
-    boxShadow: '0 5px 15px rgba(0,0,0,.5)',
-    padding: 20
-  }
-};
-
 function render(props: LoginProps & LoginActions) {
   return (
     <Modal
       aria-labelledby='modal-label'
-      style={modalStyle}
-      backdropStyle={backdropStyle}
+      ClassName="modal"
+      backdropClassName="backdrop"
       show={props.visible}
       onHide={props.dispatchLoginModalDisposeRequested}
     >
-      <div className="modal-login" style={dialogStyle()}>
+      <div className="modal-login">
         <h1>Login to Poet</h1>
         <div>
           <img onClick={props.dispatchLoginResponse}
