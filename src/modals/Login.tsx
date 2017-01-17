821ca519e1c402767c3f5103c48d3724b1852438
Esteban Ordano <eordano@gmail.com>
2017-01-17 09:21:51 -0800

    modal react-overlay

--- src/modals/Login.tsx
+++ src/modals/Login.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { Action } from 'redux';
 
-const Modal = require('react-bootstrap-modal');
+const { Modal } = require('react-overlays');
 
 import Actions from '../actions';
 
@@ -18,27 +18,47 @@ interface LoginProps {
   visible: boolean;
 }
 
+const modalStyle = {
+  position: 'fixed',
+  zIndex: 1040,
+  top: 0, bottom: 0, left: 0, right: 0
+};
+
+const backdropStyle = {
+  ...modalStyle,
+  zIndex: 'auto',
+  backgroundColor: '#000',
+  opacity: 0.5
+};
+
+const dialogStyle = () => {
+  const left = 50;
+  const top = 50;
+  return {
+    position: 'absolute',
+    width: 400,
+    top: top + '%', left: left + '%',
+    transform: `translate(-${top}%, -${left}%)`,
+    border: '1px solid #e5e5e5',
+    backgroundColor: 'white',
+    boxShadow: '0 5px 15px rgba(0,0,0,.5)',
+    padding: 20
+  }
+}
+
 function render(props: LoginProps & LoginActions) {
   return (
     <Modal
+      aria-labelledby='modal-label'
+      style={modalStyle}
+      backdropStyle={backdropStyle}
       show={props.visible}
       onHide={props.dispatchLoginModalDisposeRequested}
-      aria-labelledby="ModalHeader"
     >
-      <h1>WACHIN</h1>
-      <Modal.Header closeButton>
-        <Modal.Title id='ModalHeader'>A Title Goes here</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p>Some Content here</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Modal.Dismiss className='btn btn-default'>Cancel</Modal.Dismiss>
-
-        <button className='btn btn-primary' onClick={props.dispatchLoginResponse}>
-          Login
-        </button>
-      </Modal.Footer>
+      <div style={dialogStyle()}>
+        <h1>Test</h1>
+        <button className='btn btn-default' onClick={props.dispatchLoginResponse}>Login</button>
+      </div>
     </Modal>
   );
 }
