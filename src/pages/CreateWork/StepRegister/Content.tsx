f522c014540c87bbfcbe022c7749bc2be0e55f6e
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-01-26 17:04:57 -0500

    pages/create-work/register/content: mini style changes

--- src/pages/CreateWork/StepRegister/Content.tsx
+++ src/pages/CreateWork/StepRegister/Content.tsx
@@ -27,23 +27,17 @@ export class Content extends React.Component<ContentProps, ContentState> {
       <section className={this.props.className}>
         <h2>Content</h2>
         <form>
-          <div className="form-group row">
-            <label htmlFor={`inputContent`} className="col-sm-2 col-form-label">Content</label>
-            <div className="col-sm-10">
-              <TextUpload
-                className="text-upload"
-                buttonClassName="btn btn-secondary"
-                onChange={this.onChange.bind(this)}
-                placeholder="Content" />
-            </div>
-          </div>
+          <TextUpload
+            className="text-upload"
+            buttonClassName="btn btn-secondary"
+            onChange={this.onChange.bind(this)}
+            placeholder="Content" />
         </form>
       </section>
     )
   }
 
   private onChange(value: string) {
-    console.log('Content.onChange', value);
     this.setState({
       content: value
     })
