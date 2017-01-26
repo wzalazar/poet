02384329aac2f2d92d29823fadde2dc68636d0a2
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-01-26 17:33:10 -0500

    pages/work minifix & minirefactor

--- src/pages/Work/Overview.tsx
+++ src/pages/Work/Overview.tsx
@@ -1,19 +1,30 @@
 import * as React from 'react';
 
+import '../../extensions/Map';
+
 import WorkComponent, { WorkProps } from '../../hocs/WorkComponent';
 import ProfileLink from '../../components/ProfileLink';
 
 import './Layout.scss';
 
+function renderRow({key, value}: {key: string, value: string}) {
+  return (
+    <tr key={key}>
+      <td>{key}</td>
+      <td>{value}</td>
+    </tr>
+  );
+}
+
 function render(props: WorkProps) {
   if (!props) {
     return '';
   }
-  const tableData = new Map();
+  const tableData = new Map<string, any>();
   tableData.set('Published', props.attributes.publishedAt);
   tableData.set('Last Modified', props.attributes.lastModified);
   tableData.set('Tags', props.attributes.tags || []);
-  tableData.set('Type', props.attributes.mediaType || 'Unknown');
+  tableData.set('Type', props.attributes.articleType || 'Unknown');
 
   return (
     <div className="overview">
@@ -28,14 +39,7 @@ function render(props: WorkProps) {
           <td>Author</td>
           <td><ProfileLink id={props.author} /></td>
         </tr>
-        {
-          [...tableData.keys()].filter(key => tableData.get(key)).map(key => (
-            <tr key={key}>
-              <td>{key}</td>
-              <td>{tableData.get(key).toString()}</td>
-            </tr>
-          ))
-        }
+          { tableData.toKeyValueArray().map(renderRow) }
         </tbody>
       </table>
     </div>
