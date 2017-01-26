a5e64e76d0c131b7609df8d71f37a1b005e21efb
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-01-26 10:14:12 -0500

    use RadioButtonGroup for work filters

--- src/pages/Portfolio/WorksFilters.tsx
+++ src/pages/Portfolio/WorksFilters.tsx
@@ -1,44 +1,20 @@
 import * as React from 'react';
 
-class FilterItem {
-  name: string;
-  text: string;
-  status: boolean = false;
-
-  constructor(name: string, text: string, status: boolean = false) {
-    this.name = name;
-    this.text = text;
-    this.status = status;
-  }
-}
+import { RadioButton, RadioButtonGroup } from '../../components/RadioButtonGroup';
 
 export class PortfolioWorksFilters extends React.Component<any, undefined> {
-  readonly items: FilterItem[] = [
-    new FilterItem('all', 'All'),
-    new FilterItem('articles', 'Articles'),
-    new FilterItem('images', 'Images'),
-    new FilterItem('audio', 'Audio'),
-    new FilterItem('video', 'Video')
+  private readonly radioButtons: ReadonlyArray<RadioButton> = [
+    new RadioButton('all', 'News All'),
+    new RadioButton('articles', 'Articles'),
+    new RadioButton('images', 'Images'),
+    new RadioButton('audio', 'Audio'),
+    new RadioButton('video', 'Video')
   ];
-
-  private renderItem(filterItem: FilterItem) {
-    return (
-      <li key={filterItem.name} className="nav-item">
-        <a className={'nav-link ' + (filterItem.status ? 'active' : '')} onClick={this.onItemToggle.bind(this, filterItem)} href="#">{filterItem.text}</a>
-      </li>
-    )
-  }
 
   render() {
     return (
-      <ul className="nav nav-pills">
-        { this.items.map(item => this.renderItem(item)) }
-      </ul>
+      <RadioButtonGroup radioButtons={this.radioButtons}/>
     )
   }
 
-  private onItemToggle(filterItem: FilterItem) {
-    filterItem.status = !filterItem.status;
-    this.forceUpdate();
-  }
 }
