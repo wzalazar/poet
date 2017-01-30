5aa9a4eac285b1d618fa72c8d1b0b7c34161e625
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-01-30 16:47:00 -0500

    ImageUpload: optimization: avoid canvas.toDataURL twice

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -105,22 +105,46 @@ export class ImageUpload extends React.Component<ImageUploadProps, ImageUploadSt
   private onImageLoaded(event: any) {
     const imageData = event.target.result;
 
-    const resizedImageData = this.resizeImage(this.cropImageIntoSquareFromCenter(imageData), this.props.imageWidthLimit, this.props.imageHeightLimit);
+    let lastTime = performance.now();
+
+    const imageDataUrlToCanvas = this.imageDataUrlToCanvas(imageData);
+    console.log('imageDataUrlToCanvas', performance.now() - lastTime); lastTime = performance.now();
+
+    const cropImageIntoSquareFromCenter = this.cropImageIntoSquareFromCenter(imageDataUrlToCanvas);
+    console.log('cropImageIntoSquareFromCenter', performance.now() - lastTime); lastTime = performance.now();
+
+    const resizeImage = this.resizeImage(cropImageIntoSquareFromCenter, this.props.imageWidthLimit, this.props.imageHeightLimit);
+    console.log('resizeImage', performance.now() - lastTime); lastTime = performance.now();
+
+    const toDataURL = resizeImage.toDataURL("image/png");
+    console.log('toDataURL', performance.now() - lastTime); lastTime = performance.now();
 
     this.setState({
-      imageData: resizedImageData
+      imageData: toDataURL
     })
   }
 
-  /**
-   * Takes the data url of an image
-   * @returns imageDataUrl of the resized image
-   */
-  private resizeImage(imageDataUrl: string, maxWidth: number, maxHeight: number): string {
+  private imageDataUrlToCanvas(imageDataUrl: string): HTMLCanvasElement {
     const canvas = document.createElement('canvas');
     const image = document.createElement('img');
 
     image.src = imageDataUrl;
+
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    const canvasContext = canvas.getContext('2d');
+    canvasContext.drawImage(image, 0, 0);
+
+    return canvas;
+  }
+
+  /**
+   * Takes a canvas with an image drawn on it and returns a new canvas with the resized image.
+   * The original canvas is left unmodified.
+   */
+  private resizeImage(image: HTMLCanvasElement, maxWidth: number, maxHeight: number): HTMLCanvasElement {
+    const canvas = document.createElement('canvas');
 
     const { width: newWidth, height: newHeight } = this.scaleDownSize(image.width, image.height, maxWidth, maxHeight);
 
@@ -131,15 +155,15 @@ export class ImageUpload extends React.Component<ImageUploadProps, ImageUploadSt
 
     canvasContext.drawImage(image, 0, 0, newWidth, newHeight);
 
-    return canvas.toDataURL("image/png");
+    return canvas;
   }
 
-
-  private cropImageIntoSquareFromCenter(imageDataUrl: string) {
+  /**
+   * Takes a canvas with an image drawn on it and returns a new canvas with the cropped image.
+   * The original canvas is left unmodified.
+   */
+  private cropImageIntoSquareFromCenter(image: HTMLCanvasElement): HTMLCanvasElement {
     const canvas = document.createElement('canvas');
-    const image = document.createElement('img');
-
-    image.src = imageDataUrl;
 
     const newSize = Math.min(image.width, image.height);
 
@@ -159,7 +183,7 @@ export class ImageUpload extends React.Component<ImageUploadProps, ImageUploadSt
       newSize,
       newSize);
 
-    return canvas.toDataURL("image/png");
+    return canvas;
   }
 
   private scaleDownSize(width: number, height: number, maxWidth: number, maxHeight: number): {width: number, height: number} {
