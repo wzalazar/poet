73b0ca963392c0ce70753c6c62ba2e2bc6d92387
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-01-31 15:54:37 -0500

    pages/landing/latest-works: limit to 5

--- src/components/LatestWorks.tsx
+++ src/components/LatestWorks.tsx
@@ -3,52 +3,60 @@ import { Link } from 'react-router';
 import * as moment from 'moment';
 
 import '../extensions/String';
+
 import './LatestWorks.scss';
-import ProfileLink from './ProfileLink';
+
+import { WorkProps } from '../hocs/WorkComponent';
+
+import { ProfileLink } from './ProfileLink';
+import { ResourceProvider } from './ResourceProvider';
 
 import Config from '../config';
 
-import FetchComponent, { FetchComponentProps } from '../hocs/FetchComponent'
+type LatestWorksResource = ReadonlyArray<WorkProps>;
 
-function renderWork(props: any) {
-  return (
-    <li key={props.id} className="row py-1">
-      <div className="col-sm-8 mb-1 text-left">
-        <h3><Link to={'/works/' + props.id}>{props.attributes.name}</Link></h3>
-        <div>{ props.attributes.authorPublicKey ? <ProfileLink id={props.attributes.authorPublicKey} /> : 'Unknown author' }</div>
-      </div>
-      <div className="col-sm-4 text-right">
-        <div className="mb-1">{props.publicKey}</div>
-        <div>{moment(props.attributes.createdAt).fromNow()}</div>
-      </div>
-    </li>
-  )
+export interface LatestWorksProps {
+  readonly className?: string;
+  readonly limit?: number;
+  readonly showLink?: boolean;
 }
 
-function render(props: LatestWorksProps) {
-  return (
-    <section className="latest-works p-2">
-      <header>
-        <h5>Latest Timestamps</h5>
-        { props.showLink &&
-          <div className="more-link"><Link to="/blocks">view all »</Link></div>
-        }
-      </header>
-      <ul className="list-unstyled">
-        { props.elements.map(renderWork) }
-      </ul>
-    </section>
-  )
-}
+export default class LatestBlocks extends ResourceProvider<LatestWorksResource, LatestWorksProps, undefined> {
+  static defaultProps: LatestWorksProps = {
+    limit: 5
+  };
 
-export interface LatestWorksProps extends FetchComponentProps {
-  showLink: boolean
-}
-
-function propsToUrl(props: LatestWorksProps) {
-  return {
-    url: `${Config.api.explorer}/works`
+  renderElement(works: LatestWorksResource) {
+    return (
+      <section className="latest-works p-2">
+        <header>
+          <h5>Latest Timestamps</h5>
+          { this.props.showLink && <div className="more-link"><Link to="/blocks">view all »</Link></div> }
+        </header>
+        <ul className="list-unstyled">
+          { works.map(this.renderWork.bind(this)) }
+        </ul>
+      </section>
+    );
   }
-}
 
-export default FetchComponent.bind(null, propsToUrl, render)();
+  resourceLocator() {
+    return { url: `${Config.api.explorer}/works?limit=${this.props.limit}` }
+  }
+
+  private renderWork(props: WorkProps) {
+    return (
+      <li key={props.id} className="row py-1">
+        <div className="col-sm-8 mb-1 text-left">
+          <h3><Link to={'/works/' + props.id}>{props.attributes.name}</Link></h3>
+          <div>{ props.attributes.authorPublicKey ? <ProfileLink id={props.attributes.authorPublicKey} /> : 'Unknown author' }</div>
+        </div>
+        <div className="col-sm-4 text-right">
+          <div className="mb-1">{props.publicKey}</div>
+          <div>{moment(props.attributes.createdAt).fromNow()}</div>
+        </div>
+      </li>
+    )
+  }
+
+}
