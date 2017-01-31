985d92c3339ba4b739d17a9eea7570f9e337b9dd
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-01-31 15:33:12 -0500

    pages/landing/latest-works: limit to 5

--- src/components/LatestBlocks.tsx
+++ src/components/LatestBlocks.tsx
@@ -7,41 +7,58 @@ import './LatestBlocks.scss';
 
 import Config from '../config';
 
-import FetchComponent, { FetchComponentProps } from '../hocs/FetchComponent'
+import { ResourceProvider } from './ResourceProvider';
 
-function renderBlock(props: any) {
-  return (
-    <tr key={props.torrentHash}>
-      <td><span className="text-truncate">{props.height}</span></td>
-      <td><span className="text-truncate">{props.id
-        ? <Link to={`/blocks/${props.id}`}>{props.id.firstAndLastCharacters(4)}</Link>
-        : 'not ready'
-      }</span></td>
-      <td>{moment(props.timestamp * 1000).fromNow()}</td>
-    </tr>
-  )
+interface Block {
+  readonly torrentHash: string;
+  readonly height: string;
+  readonly id: string;
+  readonly timestamp: number;
 }
 
-function render(props: FetchComponentProps) {
-  return (
-    <table className="table table-hover latest-blocks">
-      <thead>
+type LatestBlocksResource = ReadonlyArray<Block>;
+
+export interface LatestBlocksProps {
+  readonly className?: string;
+  readonly limit?: number;
+}
+
+export default class LatestBlocks extends ResourceProvider<LatestBlocksResource, LatestBlocksProps, undefined> {
+  static defaultProps: LatestBlocksProps = {
+    limit: 5
+  };
+
+  renderElement(blocks: LatestBlocksResource) {
+    return (
+      <table className={`table table-hover latest-blocks ${this.props.className}`}>
+        <thead>
         <tr>
           <th colSpan={2}>Poet Blockchain</th>
           <th className="more-link"><Link to="/blocks">view all »</Link></th>
         </tr>
-      </thead>
-      <tbody>
-      { props.elements.map(renderBlock) }
-      </tbody>
-    </table>
-  )
-}
-
-function propsToUrl(props: FetchComponentProps) {
-  return {
-    url: `${Config.api.explorer}/blocks`
+        </thead>
+        <tbody>
+          { blocks.map(this.renderBlock.bind(this)) }
+        </tbody>
+      </table>
+    );
   }
-}
 
-export default FetchComponent(propsToUrl, render);
+  resourceLocator() {
+    return { url: `${Config.api.explorer}/blocks?limit=${this.props.limit}` }
+  }
+
+  private renderBlock(props: Block) {
+    return (
+      <tr key={props.torrentHash}>
+        <td><span className="text-truncate">{props.height}</span></td>
+        <td><span className="text-truncate">{props.id
+          ? <Link to={`/blocks/${props.id}`}>{props.id.firstAndLastCharacters(4)}</Link>
+          : 'not ready'
+        }</span></td>
+        <td>{moment(props.timestamp * 1000).fromNow()}</td>
+      </tr>
+    )
+  }
+
+}
