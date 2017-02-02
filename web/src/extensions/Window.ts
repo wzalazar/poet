fc89997ac448d229627bb2b24a6221b53271fcf3
Esteban Ordano <eordano@gmail.com>
2017-02-02 10:49:29 -0500

    Drop fetch from polyfill

--- web/src/extensions/Window.ts
+++ web/src/extensions/Window.ts
@@ -1,11 +1,7 @@
 import { StoreCreator } from 'redux';
-import * as isoFetch from 'isomorphic-fetch';
 
 declare global {
   interface Window {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: () => StoreCreator
-    fetch: typeof isoFetch
   }
-
-  const fetch: typeof window.fetch; // TODO: remove once https://github.com/Microsoft/TypeScript/pull/12493 is merged
 }
