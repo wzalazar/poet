8a76eec4d1cfbfdd1db09a5ce5faa3d92d6e32aa
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-02-09 19:58:47 -0300

    images

---
+++ web/src/images/Images.tsx
@@ -0,0 +1,8 @@
+export class Images {
+  static Logo = require('./logo.svg');
+  static InvertedLogo = require('./logo-inverted.svg');
+  static Twitter = require('./twitter.svg');
+  static Github = require('./github.svg');
+  static Reddit = require('./reddit.svg');
+  static Slack = require('./slack.svg');
+}
