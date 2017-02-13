31c5d6cbcb8c71b03e0b79ae28a3ca009d0abd60
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-02-13 16:42:42 -0300

    NavBar: hide only if on landing and logged-in

--- web/src/templates/RootLayout.tsx
+++ web/src/templates/RootLayout.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 
+import Constants from '../constants';
 import { Navbar } from "../organisms/Navbar";
 import { UserNavbar } from "../organisms/UserNavbar";
 import { Footer } from '../organisms/Footer';
@@ -9,22 +11,42 @@ import './RootLayout.scss'
 
 const modals = Modals.map((Modal, index) => <Modal key={index}/>);
 
-const userPaths = ['/account/settings', '/account/wallet'];
+interface RootLayoutProps {
+  readonly loggedIn?: boolean;
+  readonly location?: {
+    readonly pathname: string;
+  }
+  readonly children?: any;
+}
 
-export function Layout(props: any) {
+function render(props: RootLayoutProps) {
   // TODO: a way for pages to provide these so the RootLayout doesn't need to know what urls are mapped to what pages
-  const navbarShadow = !['/works', '/'].includes(props.location.pathname);
-  const navbarTransparent = ['/'].includes(props.location.pathname);
-  const displayNavbarLogo = !['/'].includes(props.location.pathname);
-  const displayNavbarSearch = !['/'].includes(props.location.pathname);
+  const navbarShadow = !['/works', '/'].includes(props.location.pathname) || !props.loggedIn;
+  const navbarTransparent = ['/'].includes(props.location.pathname) && props.loggedIn;
+  const displayNavbarLogo = !['/'].includes(props.location.pathname) || !props.loggedIn;
+  const displayNavbarSearch = !['/'].includes(props.location.pathname) || !props.loggedIn;
+  const displayUserNavbar = ['/account/settings', '/account/wallet'].includes(props.location.pathname);
 
   return (
     <div className="root-layout">
       { modals }
-      <Navbar shadow={navbarShadow} displayLogo={displayNavbarLogo} displaySearch={displayNavbarSearch} transparent={navbarTransparent} />
-      { userPaths.includes(props.location.pathname) && <UserNavbar location={props.location.pathname}/> }
+      <Navbar
+        shadow={navbarShadow}
+        displayLogo={displayNavbarLogo}
+        displaySearch={displayNavbarSearch}
+        transparent={navbarTransparent}
+      />
+      { displayUserNavbar && <UserNavbar location={props.location.pathname}/> }
       { props.children }
       <Footer/>
     </div>
   );
 }
+
+function mapStateToProps(state: any): RootLayoutProps {
+  return {
+    loggedIn: state.session.state === Constants.LOGGED_IN
+  }
+}
+
+export const Layout = connect(mapStateToProps)(render);
