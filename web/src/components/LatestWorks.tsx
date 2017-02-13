268fb467a17766bc252d212d4d5c07fac55d8d80
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-02-13 11:38:14 -0300

    LatestWorks styling

--- web/src/components/LatestWorks.tsx
+++ web/src/components/LatestWorks.tsx
@@ -28,15 +28,21 @@ export default class LatestBlocks extends ResourceProvider<LatestWorksResource, 
 
   renderElement(works: LatestWorksResource) {
     return (
-      <section className="latest-works p-2">
-        <header>
-          <h5>Latest Timestamps</h5>
-          { this.props.showLink && <div className="more-link"><Link to="/blocks">view all »</Link></div> }
-        </header>
-        <ul className="list-unstyled">
+      <table className="latest-works">
+        <thead>
+          <tr>
+            <td className="title" colSpan={2}>
+              Latest Works
+            </td>
+            <td className="view-latest">
+              { this.props.showLink && <Link to="/blocks">View Latest »</Link> }
+            </td>
+          </tr>
+        </thead>
+        <tbody>
           { works.map(this.renderWork.bind(this)) }
-        </ul>
-      </section>
+        </tbody>
+      </table>
     );
   }
 
@@ -46,16 +52,18 @@ export default class LatestBlocks extends ResourceProvider<LatestWorksResource, 
 
   private renderWork(props: WorkProps) {
     return (
-      <li key={props.id} className="row py-1">
-        <div className="col-sm-8 mb-1 text-left">
-          <h3><Link to={'/works/' + props.id}>{props.attributes.name}</Link></h3>
-          <div>{ props.attributes.authorPublicKey ? <ProfileLink id={props.attributes.authorPublicKey} /> : 'Unknown author' }</div>
-        </div>
-        <div className="col-sm-4 text-right">
-          <div className="mb-1">{props.publicKey}</div>
-          <div>{moment(props.attributes.createdAt).fromNow()}</div>
-        </div>
-      </li>
+      <tr key={props.id}>
+        <td className="work-name">
+          <Link to={'/works/' + props.id}>{props.attributes.name}</Link>
+          {/*<div className="author">{ props.attributes.authorPublicKey ? <ProfileLink id={props.attributes.authorPublicKey} /> : 'Unknown author' }</div>*/}
+        </td>
+        <td className="id">
+          {props.id && props.id.firstAndLastCharacters(4)}
+        </td>
+        <td className="date">
+          {moment(props.attributes.createdAt).fromNow()}
+        </td>
+      </tr>
     )
   }
 
