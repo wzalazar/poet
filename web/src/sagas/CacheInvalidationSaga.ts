55497eeed9903cfeb828a577917cf393ad5b2bf0
Esteban Ordano <eordano@gmail.com>
2017-02-16 13:23:04 -0800

    Invalidate profile - closes #29

--- web/src/sagas/CacheInvalidationSaga.ts
+++ web/src/sagas/CacheInvalidationSaga.ts
@@ -4,6 +4,8 @@ import { put } from 'redux-saga/effects'
 import { Config } from '../config';
 import Actions from '../actions/index'
 import { FetchType } from '../reducers/FetchReducer';
+import { currentPublicKey } from '../selectors/session';
+import { select } from 'redux-saga/effects';
 
 function* claimsSubmittedSuccess(claimSubmittedAction: any) {
   for (let claim of claimSubmittedAction.claims) {
@@ -11,6 +13,10 @@ function* claimsSubmittedSuccess(claimSubmittedAction: any) {
       const shortUrl = '/works';
       const url = Config.api.explorer + shortUrl;
       yield put({ type: `clear ${shortUrl}`, fetchType: FetchType.CLEAR, url });
+    }
+    if (claim.type === 'Profile') {
+      const publicKey = yield select(currentPublicKey)
+      yield put({ type: Actions.fetchProfileData, profilePublicKey: publicKey });
     }
   }
 }
