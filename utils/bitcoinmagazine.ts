d3fda4fdb65bef9e169a7dfc74a4d38ba94d6427
Esteban Ordano <eordano@gmail.com>
2017-02-21 09:03:04 -0800

    Keep localhost on btcmagazine pull

--- utils/bitcoinmagazine.ts
+++ utils/bitcoinmagazine.ts
@@ -12,8 +12,8 @@ declare var require: any
 const bitcore = require('bitcore-lib')
 
 const targetURL = 'https://bitcoinmagazine.com/feed/'
-const explorerURL = 'https://poet.host/api/explorer'
-const publisherURL = 'https://poet.host/api/user'
+const explorerURL = 'http://localhost:10000/api/explorer'
+const publisherURL = 'http://localhost:10000/api/user'
 
 const rawKey = '4cbfeb0cbfa891148988a50b549c42309e088a7839dd14ab480f542286725d3a'
 
