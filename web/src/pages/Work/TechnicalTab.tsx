4d8b7fd6d34d5316cfb24d064d29e269a9d696f8
Esteban Ordano <eordano@gmail.com>
2017-02-21 06:40:34 -0800

    technical tab first stab - closes #45

--- web/src/pages/Work/TechnicalTab.tsx
+++ web/src/pages/Work/TechnicalTab.tsx
@@ -7,6 +7,20 @@ function render(props: Work): JSX.Element {
   return (
     <div className="technicalTab">
       <h1>Technical</h1>
+      <table>
+        <tbody>
+        {
+          Object.keys(props.claimInfo)
+            .filter(info => info !== 'id')
+            .map(info => {
+              return (<tr>
+                <td>{ info }</td><td>{ (props.claimInfo as any)[info] }</td>
+                </tr>
+              )
+            })
+        }
+        </tbody>
+      </table>
     </div>
   )
 }
