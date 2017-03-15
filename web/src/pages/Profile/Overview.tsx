62dc3ac66adf70513d9b8de35c9286b3bda675eb
Esteban Ordano <eordano@gmail.com>
2017-03-15 12:34:17 -0700

    Fix public page for no profile cases - improves on #133

--- web/src/pages/Profile/Overview.tsx
+++ web/src/pages/Profile/Overview.tsx
@@ -2,15 +2,17 @@ import * as React from 'react';
 import ProfileComponent, { ProfileProps } from '../../hocs/ProfileComponent';
 
 import './Overview.scss';
+import { Images } from '../../images/Images';
 
 function render(props: ProfileProps) {
   return (
     <div className="overview col-sm-3">
-      <div className="avatar"><img src={props.attributes.imageData}/></div>
-      <h2>{props.attributes.displayName}</h2>
-      <div className="bio">A short description of the publishers name and background</div>
+      <div className="avatar"><img src={props.attributes && props.attributes.imageData || Images.Anon}/></div>
+      <h2>{props.attributes && props.attributes.displayName || 'Anonymous'}</h2>
+      <div className="bio">{ props.attributes && props.attributes.bio }</div>
+      <div className="publicKey">Identifier: <pre>{ props.id }</pre></div>
       <ul>
-        { props.attributes.email && <li><div>{props.attributes.email}</div></li> }
+        { props.attributes && props.attributes.email && <li><div>{props.attributes.email}</div></li> }
       </ul>
     </div>
   )
