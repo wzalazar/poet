744363ffecc288d8c19b161a02a806dfaeb4068b
Alvaro Crespo <alvaro.andres.crespo@gmail.com>
2017-03-31 18:23:01 -0300

    Code guidelines Refactor. Removed ;.

--- node/src/notifications/appserver.ts
+++ node/src/notifications/appserver.ts
@@ -1,30 +1,30 @@
-const express  = require('express');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-//var io = require('socket.io');
-const fcm = require('fcm-node');
+const express  = require('express')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+//var io = require('socket.io')
+const fcm = require('fcm-node')
 
-import "reflect-metadata";
-import * as path from 'path';
-import { createConnection } from 'typeorm';
+import "reflect-metadata"
+import * as path from 'path'
+import { createConnection } from 'typeorm'
 import Device from './device'
 
-const SERVER_API_KEY='YOUR_SERVER_API_KEY';
-const registrationToken = 'FIREBASE_EXAMPLE_REGISTRATION_TOKEN';
-const fcmCli = new fcm(SERVER_API_KEY);
-const app      = express();
-const port     = process.env.PORT || 8080;
+const SERVER_API_KEY='YOUR_SERVER_API_KEY'
+const registrationToken = 'FIREBASE_EXAMPLE_REGISTRATION_TOKEN'
+const fcmCli = new fcm(SERVER_API_KEY)
+const app      = express()
+const port     = process.env.PORT || 8080
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(logger('dev'));
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json())
+app.use(logger('dev'))
 
-const listen = app.listen(port);
-//var socket = io.listen(listen);
+const listen = app.listen(port)
+//var socket = io.listen(listen)
 
-//require('./routes/routes')(app,socket);
+//require('./routes/routes')(app,socket)
 
-console.log('The App runs on port ' + port);
+console.log('The App runs on port ' + port)
 
 var payloadOK = {
     to: registrationToken,
@@ -38,33 +38,33 @@ var payloadOK = {
     notification: { //notification object
         title: 'HELLO', body: 'World!', sound : "default", badge: "1"
     }
-};
+}
 
 const callbackLog = function (sender: string, err: any, res: any) {
     console.log("\n__________________________________")
-    console.log("\t" + sender);
+    console.log("\t" + sender)
     console.log("----------------------------------")
-    console.log("err=" + err);
-    console.log("res=" + res);
-    console.log("----------------------------------\n>>>");
-};
+    console.log("err=" + err)
+    console.log("res=" + res)
+    console.log("----------------------------------\n>>>")
+}
 
 app.post('/register',function(req: any, res: any) {
 
-    const deviceName = req.body.deviceName;
-    const deviceId   = req.body.deviceId;
-    const registrationId = req.body.registrationId;
-    const platform = req.body.platform;
-    const pubKey = req.body.pubKey;
+    const deviceName = req.body.deviceName
+    const deviceId   = req.body.deviceId
+    const registrationId = req.body.registrationId
+    const platform = req.body.platform
+    const pubKey = req.body.pubKey
 
     if (typeof pubKey  == 'undefined' || typeof deviceId == 'undefined' || typeof registrationId  == 'undefined') {
         //TODO error handling
-        // console.log(constants.error.msg_invalid_param.message);
-        // res.json(constants.error.msg_invalid_param);
+        // console.log(constants.error.msg_invalid_param.message)
+        // res.json(constants.error.msg_invalid_param)
     } else if (!pubKey.trim() || !deviceId.trim() || !registrationId.trim()) {
         //TODO error handling
-        // console.log(constants.error.msg_empty_param.message);
-        // res.json(constants.error.msg_empty_param);
+        // console.log(constants.error.msg_empty_param.message)
+        // res.json(constants.error.msg_empty_param)
     } else {
 
         createConnection({
@@ -89,33 +89,33 @@ app.post('/register',function(req: any, res: any) {
             device.publicKey = pubKey
             device.registrationId = registrationId
 
-            let deviceRepository = connection.getRepository(Device);
-            await deviceRepository.persist(device);
+            let deviceRepository = connection.getRepository(Device)
+            await deviceRepository.persist(device)
 
-            connection.close();
+            connection.close()
             res.end("It works!!")
-            console.log("Device has been saved");
+            console.log("Device has been saved")
         }).catch(error => {
             console.log(error)
             res.end("It didn't work :(!!")
-        });
+        })
     }
-});
+})
 
 app.post('/send', function(req: any, res: any) {
 	
-	var message = req.body.message;
-	var registrationId = req.body.registrationId;
+	var message = req.body.message
+	var registrationId = req.body.registrationId
 
-	console.log("message=" + message);
-	console.log("registrationId=" + registrationId);
+	console.log("message=" + message)
+	console.log("registrationId=" + registrationId)
 
-	payloadOK.to = registrationId;
-	payloadOK.notification.body = message;
+	payloadOK.to = registrationId
+	payloadOK.notification.body = message
 
 	fcmCli.send(payloadOK, function(err: any, res: any) {
-		callbackLog('sendOK', err, res);
+		callbackLog('sendOK', err, res)
 		res.end("It works!!")
-	});
-});
+	})
+})
 
--- node/src/notifications/example.ts
+++ node/src/notifications/example.ts
@@ -1,13 +1,13 @@
 /**
  * Created by Leonardo on 02/08/2016.
  */
-const FCM = require('fcm-node');
 
-const SERVER_API_KEY='YOUR_SERVER_API_KEY';
+const FCM = require('fcm-node')
 
-var validDeviceRegistrationToken = 'c1m7I:A ... bjj4SK-'; //put a valid device token here
+const SERVER_API_KEY='YOUR_SERVER_API_KEY'
+const validDeviceRegistrationToken = 'c1m7I:A ... bjj4SK-' //put a valid device token here
 
-var fcmCli= new FCM(SERVER_API_KEY);
+var fcmCli= new FCM(SERVER_API_KEY)
 
 var payloadOK = {
     to: validDeviceRegistrationToken,
@@ -21,7 +21,7 @@ var payloadOK = {
     notification: { //notification object
         title: 'HELLO', body: 'World!', sound : "default", badge: "1"
     }
-};
+}
 
 var payloadError = {
     to: "4564654654654654", //invalid registration token
@@ -31,7 +31,7 @@ var payloadError = {
     priority: 'high',
     content_available: true,
     notification: { title: 'TEST HELLO', body: '123', sound : "default", badge: "1" }
-};
+}
 
 var payloadMulticast = {
     registration_ids:["4564654654654654",
@@ -44,37 +44,37 @@ var payloadMulticast = {
     priority: 'high',
     content_available: true,
     notification: { title: 'Hello', body: 'Multicast', sound : "default", badge: "1" }
-};
+}
 
 var callbackLog = function (sender: string, err: any, res: any) {
     console.log("\n__________________________________")
-    console.log("\t" + sender);
+    console.log("\t" + sender)
     console.log("----------------------------------")
-    console.log("err=" + err);
-    console.log("res=" + res);
-    console.log("----------------------------------\n>>>");
-};
+    console.log("err=" + err)
+    console.log("res=" + res)
+    console.log("----------------------------------\n>>>")
+}
 
 function sendOK()
 {
     fcmCli.send(payloadOK, function(err: any, res: any) {
-        callbackLog('sendOK', err, res);
-    });
+        callbackLog('sendOK', err, res)
+    })
 }
 
 function sendError() {
     fcmCli.send(payloadError, function(err: any, res: any) {
-        callbackLog('sendError', err, res);
-    });
+        callbackLog('sendError', err, res)
+    })
 }
 
 function sendMulticast(){
     fcmCli.send(payloadMulticast, function(err: any, res: any) {
-        callbackLog('sendMulticast', err, res);
-    });
+        callbackLog('sendMulticast', err, res)
+    })
 }
 
 
-sendOK();
-sendMulticast();
-sendError();
+sendOK()
+sendMulticast()
+sendError()
