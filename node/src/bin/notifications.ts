f2f731147f67e13b81dcd57860d054535a0ab56f
Esteban Ordano <eordano@gmail.com>
2017-04-11 22:42:58 +0000

    Firebase key and fix

--- node/src/bin/notifications.ts
+++ node/src/bin/notifications.ts
@@ -1,3 +1,3 @@
 import { default as createServer } from '../notifications/appserver'
 
-createServer('', 5500)
+createServer('AAAAPFVqv4w:APA91bHPvCWqiko048XLlcugeSMXzYX5nS4GyBz8VYDOhsuqNrKPDYhj5XMz31kIGs3t56WBDok05fKw0ddKLMQWcdXNLNfI63eFka6juXEGZgWwiDvJiORAy70Op8zNfXswzNvd-7Sz', 5500)
