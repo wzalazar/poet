71d4b8ab4d296191f9d773feee551803da8b2d49
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-05-30 01:17:13 -0300

    code cleanup

--- web/src/pages/Account/Wallet/BitcoinToCurrency.tsx
+++ web/src/pages/Account/Wallet/BitcoinToCurrency.tsx
@@ -1,20 +1,22 @@
-import * as React from 'react';
+// TODO: move this file to components/atoms/
 
-import { Configuration } from '../../../configuration';
-import { ResourceProvider, ResourceLocator } from '../../../components/ResourceProvider';
+import * as React from 'react'
+
+import { Configuration } from 'configuration'
+import { ResourceProvider, ResourceLocator } from 'components/ResourceProvider'
 
 export interface BitcoinToCurrencyProps {
-  readonly currency: string;
-  readonly amount: number;
+  readonly currency: string
+  readonly amount: number
 }
 
 export interface BlockchainTicker {
   readonly [currency: string]: {
-    '15m': number;
-    last: number;
-    buy: number;
-    sell: number;
-    symbol: string;
+    '15m': number
+    last: number
+    buy: number
+    sell: number
+    symbol: string
   }
 }
 
@@ -31,6 +33,6 @@ export class BitcoinToCurrency extends ResourceProvider<BlockchainTicker, Bitcoi
       return <span>Ticker information not available in currency {this.props.currency}</span>;
 
     return <span>{(blockchainTicker[this.props.currency].last * this.props.amount).toFixed(2)}</span>
-
   }
+
 }
