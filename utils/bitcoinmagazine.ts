851b2729800e64506bdb43a44d1fa94e39f6b90d
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-06-15 01:01:56 -0300

    change explorer and publisher url to alpha.po.et

--- utils/bitcoinmagazine.ts
+++ utils/bitcoinmagazine.ts
@@ -12,8 +12,8 @@ declare var require: any
 const bitcore = require('bitcore-lib')
 
 const targetURL = 'https://bitcoinmagazine.com/feed/'
-const explorerURL = 'https://poet.host/api/explorer'
-const publisherURL = 'https://poet.host/api/user'
+const explorerURL = 'https://alpha.po.et/api/explorer'
+const publisherURL = 'https://alpha.po.et/api/user'
 
 const rawKey = '4cbfeb0cbfa891148988a50b549c42309e088a7839dd14ab480f542286725d3a'
 
