d623cc0fa7f868ed14bc20ad3bf0555757b59092
Esteban Ordano <eordano@gmail.com>
2017-08-04 14:35:53 -0300

    add notary validation

--- node/src/blockchain/certificatonService.ts
+++ node/src/blockchain/certificatonService.ts
@@ -1,4 +1,4 @@
-import { Claim } from 'poet-js'
+import { Claim, verifies, doubleSha } from 'poet-js'
 
 import { BlockMetadata } from '../events'
 import { CertificationRules } from './rules/certification'
@@ -10,12 +10,20 @@ export class CertificationService {
 
   constructor(domainService: BlockchainService) {
     this.domainService = domainService
+    this.notaries = [
+      '031c26b8b364e2970f3cefa458217c9d71e945576930509b48b7a9c44a88ef7464'
+    ]
   }
 
   async notaryApproval(claim: Claim) {
-    // TODO: Validate signature
-    // TODO: Validate public key is trusted
     // TODO: Notary repository
+    if (!this.notaries.includes(claim.publicKey)) {
+      return false
+    }
+    const encoded = new Buffer(ClaimBuilder.getEncodedForSigning(claim), 'hex')
+    if (!verifies(doubleSha, encoded, claim.signature, claim.publicKey)) {
+      return false
+    }
     return true
   }
 
