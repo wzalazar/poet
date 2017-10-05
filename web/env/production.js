8ace36833a6ec97fa0ab02660d3ad1e42a7be31a
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-10-05 14:55:06 -0300

    env

---
+++ web/env/production.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "api": {
+    "explorer": "/api/explorer/production",
+    "user": "/api/user",
+    "auth": {
+      "host": "http://localhost:3000",
+      "path": "/api/auth"
+    },
+    "mockApp": "/api/mockApp",
+    "blockchain": "https://blockchain.info"
+  },
+  "imageUpload": {
+    "maxWidth": 100,
+    "maxHeight": 100
+  },
+  "dateFormat": "MMMM Do YYYY",
+  "dateTimeFormat": "MMMM Do YYYY, HH:mm:ss",
+  "pagination": {
+    "limit": 10,
+    "visiblePageCount": 6
+  },
+  "useMockSigner": true
+}
